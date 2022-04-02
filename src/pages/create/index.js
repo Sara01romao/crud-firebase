30865400015a8be23d16a35b7d024c4ff9b3596a
Sara Romão <sara-romao@live.com>
2022-04-02 14:46:51 -0300

    pg para cadastrar

---
+++ src/pages/create/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Create() {
+  return (
+    <div className={` container`}>
+
+      <h1 className='title'>Cadastrar</h1>
+      
+      
+     
+    </div>
+  )
+}
