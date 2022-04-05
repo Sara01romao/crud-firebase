b4d832d14c82ecd2f9414ba8cb3cfb285c0f28bb
Sara Romão <sara-romao@live.com>
2022-04-05 11:52:33 -0300

    ajuste container e elementos

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,12 +5,16 @@ import styles from './styles.module.css'
 
 export default function Home() {
   return (
-    <div className={`${styles.containerHome} container`}>
+    <div className={`${styles.containerHome} `}>
 
-      <h1 className='title'>Lista</h1>
+      <div className={styles.info}>
+        <h1 className='title'>Lista</h1>
+        
+        <Link to="/cadastrar">+ Adicionar</Link>
+        <Lista/>
+      </div>
+
       
-      <Link to="/cadastrar">+ Adicionar</Link>
-      <Lista/>
     </div>
   )
 }
