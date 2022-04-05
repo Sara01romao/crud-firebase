2352a8f53f67a5cfa3eccba1197da1d0cdf8edf1
Sara Romão <sara-romao@live.com>
2022-04-05 11:51:27 -0300

    estrutura remover item e estilo

--- src/components/Remove/index.js
+++ src/components/Remove/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import firebase from '../../services/firebaseConnection';
+import styles from './styles.module.css';
+import Trash from '../../assets/trash.svg'
 
 export default function RemoveModal({item, setValue}) {
 
@@ -22,9 +24,24 @@ export default function RemoveModal({item, setValue}) {
    
 
   return (
-    <div>Modal{item}
+    <div className={styles.modal}>
 
-        <button onClick={() => remover(item)}>Exluir</button>
+      <div className={styles.modalContainer}>
+           <img src={Trash} alt="Icon Lixeira"></img>
+
+          <div className={styles.info}>
+            <h3>Excluir Caixa <span>Nº {item}</span></h3>
+            <p>Tem certeza que você deseja excluir esta caixa?</p>
+          </div>
+         
+          <div className={styles.options}>
+            <button className={styles.btnClose} onClick={()=> setValue(null)}>Cancelar</button>
+            <button className={styles.btnRemove} onClick={() => remover(item)}>Exluir</button>
+          </div>
+          
+      </div>
+
+        
     </div>
   )
 }
