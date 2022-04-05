68333f2789ee37c646ba72d6632ec07e0d2839c9
Sara Romão <sara-romao@live.com>
2022-04-05 13:56:15 -0300

    remove item

--- src/services/firebaseConnection.js
+++ src/services/firebaseConnection.js
@@ -5,13 +5,7 @@ import "firebase/firestore";
 
 
 let firebaseConfig = {
-    apiKey: "AIzaSyAieMwFNt34quaHvrD2F6aNhLN2Vl4gd2M",
-    authDomain: "abelhas-b4bd8.firebaseapp.com",
-    projectId: "abelhas-b4bd8",
-    storageBucket: "abelhas-b4bd8.appspot.com",
-    messagingSenderId: "304824868355",
-    appId: "1:304824868355:web:c321d826291ec171f33836",
-    measurementId: "G-HLNSNDLVNF"
+    
   
     
 };
